9422fc23a30174fdac399cf0e56510ec0247966d
Afrasiyab <afrasiyabmanzoir709@gmail.com>
2023-01-08 11:33:39 +0500

    Add files via upload

---
+++ timeConversion.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    hour = int(s[0:2])
+    if s[8] == 'A':
+        hour = str(hour % 12) if hour == 10 or hour == 11 else "0" + str(hour % 12);
+    else:
+        hour = str(hour) if hour == 12 else str(hour + 12)
+    return hour + s[2:8]
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
